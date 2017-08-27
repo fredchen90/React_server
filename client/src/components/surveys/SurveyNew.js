a179e37a691f06171c22c0cb5965207c99410013
Fred <fredchen90@gmail.com>
2017-08-27 13:11:38 +0800

    Button back for surveyformreview.

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -15,8 +15,12 @@ class SurveyNew extends Component {
   state = { showFormReview: false };
 
   renderContent() {
-    if (this.state.showFormReview === true) {
-      return <SurveyFormReview />;
+    if (this.state.showFormReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
     }
 
     return (
