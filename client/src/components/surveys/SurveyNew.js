512c2c9fb4ea6543ad13ce788c78ebe0b877f125
Fred <fredchen90@gmail.com>
2017-08-27 12:02:43 +0800

    Basic surveyform click button next to surveyformreview.

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,13 +1,38 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
-	render() {
-		return (
-			<SurveyForm />
-		);
-	}
+  // constructor(props) {
+  // 	super(props);
+  //
+  // 	this.state = { showFormReview: false };
+  // }
+
+  // Create react application use babel pluginm
+  // equal above constructor
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview === true) {
+      return <SurveyFormReview />;
+    }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderContent()}
+      </div>
+    );
+  }
 }
 
 export default SurveyNew;
