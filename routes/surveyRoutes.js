f11f4850a9fc965562e81c3edfcce8603b4e1673
Fred <fredchen90@gmail.com>
2017-08-29 22:54:44 +0800

    Fix webhook error.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting');
   });
 
@@ -26,20 +26,23 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
-      .each({ surveyId, emailm, choice }) => {
+      .each(({ surveyId, email, choice }) => {
         Survey.updateOne(
           {
             _id: surveyId,
             recipients: {
-              $eleMatch: { eamil: email, responded: false }
+              $elemMatch: { eamil: email, responded: false }
             }
-          }, {
+          },
+          {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded'; true }
-          }).exec();
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
       })
       .value();
-
+    console.log(req.body);
     res.send({});
   });
 
