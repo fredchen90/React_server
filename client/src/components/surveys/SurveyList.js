6f7a1b4e309da59dd81d7fe520ebf12bceeecc06
fred.chen <fred.chen@latticeworkinc.com>
2017-08-30 17:52:36 +0800

    Fix wrong attribute.

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -11,8 +11,8 @@ class SurveyList extends Component {
 		return this.props.surveys.reverse().map(survey => {
 			return (
 				<div className="card darken-1" key={survey._id}>
-					<div class="card-content white-text">
-						<span class="card-title">{survey.title}</span>
+					<div className="card-content">
+						<span className="card-title">{survey.title}</span>
 						<p>
 							{survey.body}
 						</p>
