0107593995ee9ac2bff332d46a401a43ca407c1b
fred.chen <fred.chen@latticeworkinc.com>
2017-08-31 11:24:07 +0800

    Config stripe checkout language to en.

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -10,6 +10,7 @@ class Payments extends Component {
         name="Emaily"
         description="$5 for 5 email credit"
         amount={500}
+        locale="en"
         token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}>
         <button className="btn">Add Credits</button>
